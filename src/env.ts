f08aab13f7bf9533f4fdfdcf8ec61ebaf5a6a60a
Pedro Oliveira <Pedroaolive@gmail.com>
2024-07-07 19:04:20 -0300

    chore: add validation for delay env

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 const envSchema = z.object({
   VITE_API_URL: z.string().url(),
+  VITE_API_REQUEST_DELAY: z.string().transform((value) => value === "true"),
 });
 
 // import.meta do Vite é igual ao process.env do Node
