a8919c08250260b2687cf9ad7f40c58b3b74e781
Pedro Oliveira <Pedroaolive@gmail.com>
2024-07-07 19:06:14 -0300

    chore: add interceptor for env delay

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,4 +3,13 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
+  withCredentials: true,
 });
+
+if (env.VITE_API_REQUEST_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => setTimeout(resolve, 2200));
+
+    return config;
+  });
+}
