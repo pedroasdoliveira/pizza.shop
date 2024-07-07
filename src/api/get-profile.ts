a2e11dd146fd3353e68ad8703a4842c728eb60e1
Pedro Oliveira <Pedroaolive@gmail.com>
2024-07-07 19:04:58 -0300

    feat: add getProfile response api

---
+++ src/api/get-profile.ts
@@ -0,0 +1,17 @@
+import { api } from "@/lib/axios";
+
+export interface IGetProfileResponse {
+  id: string;
+  name: string;
+  email: string;
+  phone: string | null;
+  role: "manager" | "customer";
+  createdAt: Date | null;
+  updatedAt: Date | null;
+}
+
+export const getProfile = async (): Promise<IGetProfileResponse> => {
+  const response = await api.get<IGetProfileResponse>("/me");
+
+  return response.data;
+};
