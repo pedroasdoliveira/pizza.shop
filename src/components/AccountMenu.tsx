656e135eb55b17f707accd63a4337307582251ba
Pedro Oliveira <Pedroaolive@gmail.com>
2024-09-28 15:00:19 -0300

    feat: add func for sign-out account

--- src/components/AccountMenu.tsx
+++ src/components/AccountMenu.tsx
@@ -8,14 +8,19 @@ import {
   DropdownMenuItem,
   DropdownMenuSeparator,
 } from "./ui/dropdown-menu";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { getProfile } from "@/api/get-profile";
 import { getManagedRestaurant } from "@/api/get-managed-restaurant";
 import { Skeleton } from "./ui/skeleton";
 import { Dialog, DialogTrigger } from "./ui/dialog";
 import StoreProfileDialog from "./Store-profile-dialog";
+import { useNavigate } from "react-router-dom";
+import { signOut } from "@/api/sign-out";
+import { Paths } from "@/routes";
 
 const AccountMenu = () => {
+  const navigate = useNavigate();
+
   const { data: profile, isLoading: profileLoading } = useQuery({
     queryKey: ["profile"],
     queryFn: getProfile,
@@ -28,6 +33,13 @@ const AccountMenu = () => {
       queryFn: getManagedRestaurant,
       staleTime: Infinity,
     });
+
+  const { mutateAsync: signOutFn, isPending: isSigningOut } = useMutation({
+    mutationFn: signOut,
+    onSuccess: () => {
+      navigate(Paths.SIGN_IN, { replace: true });
+    },
+  });
 
   return (
     <Dialog>
@@ -75,10 +87,16 @@ const AccountMenu = () => {
             </DropdownMenuItem>
           </DialogTrigger>
 
-          <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-            <LogOut className="mr-2 h-4 w-4" />
+          <DropdownMenuItem
+            asChild
+            className="text-rose-500 dark:text-rose-400"
+            disabled={isSigningOut}
+          >
+            <button className="w-full" onClick={() => signOutFn()}>
+              <LogOut className="mr-2 h-4 w-4" />
 
-            <span>Sair</span>
+              <span>Sair</span>
+            </button>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
