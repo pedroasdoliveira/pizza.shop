7186f161c18b520563a4c5dab2effcd2f7b8bb2c
Pedro Oliveira <Pedroaolive@gmail.com>
2024-10-05 17:22:01 -0300

    test: add unit test for NavLink component

---
+++ src/components/tests/Nav-link.spec.tsx
@@ -0,0 +1,27 @@
+import { render } from "@testing-library/react";
+import NavLink from "../NavLink";
+import { MemoryRouter } from "react-router-dom";
+
+describe("Nav link component", () => {
+  it("should highlight the nav link when is the current page link", () => {
+    const { debug, getByText } = render(
+      <>
+        <NavLink to={"/"}>Home</NavLink>
+
+        <NavLink to={"/about"}>About</NavLink>
+      </>,
+      {
+        wrapper: ({ children }) => {
+          return (
+            <MemoryRouter initialEntries={["/about"]}>{children}</MemoryRouter>
+          );
+        },
+      },
+    );
+
+    debug();
+
+    expect(getByText("Home").dataset.current).toEqual("false");
+    expect(getByText("About").dataset.current).toEqual("true");
+  });
+});
